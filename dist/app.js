a42d88c0ebddf4d9ff121558a155c657820b340c
sajid365-sr <sajidsorker86@gmail.com>
2022-12-17 19:53:38 +0600

    project setup

---
+++ dist/app.js
@@ -0,0 +1,4 @@
+"use strict";
+var message = 'Hello Typescript';
+var message2 = 'Bye JavaScript';
+console.log(message, message2, language);
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+
+const message = 'Hello Typescript';
+const message2 = 'Bye JavaScript';
+console.log(message, message2, language);
