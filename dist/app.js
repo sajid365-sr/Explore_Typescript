d6166c28b98666f5e7e52e35cf9dee99fb778b90
sajid365-sr <sajidsorker86@gmail.com>
2022-12-17 20:42:52 +0600

    primitive types, inferred types, union types

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,22 @@
 "use strict";
+// Project setup
 var message = 'Hello Typescript';
 var message2 = 'Bye JavaScript';
-console.log(message, message2, language);
+// console.log(message, message2, language);
+// Primitive Types(string, number and boolean),
+// unionTypes and how inferred types in typescript.
+// infer means TS auto dhore nibe konta kon type (ex: string, boolean, number). Jodi auto infer korte na pare tahole amader manually bole dite hobe konta kon types.
+//  let myName:string;
+//  myName = 'sajid';
+// myName = 'sorker';
+// myName=34;
+// myName=false;
+// let age:number;
+// age = 34;
+// age = '45';
+// unionTypes means aker odhik type use kora jabe ak variable a. ar jonno age theke e bole dite hobe kon kon type rakha hobe.
+var age = 22; // akhane bole dewa hocche j string & number 2 tai rakha jabe
+age = '45';
+// const myName:'sajid' = 'sajid';
+var myName = 'sajid';
+myName = "sorker";
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,35 @@
 
+
+// Project setup
+
 const message = 'Hello Typescript';
 const message2 = 'Bye JavaScript';
-console.log(message, message2, language);
+// console.log(message, message2, language);
+
+// Primitive Types(string, number and boolean),
+// unionTypes and how inferred types in typescript.
+
+ // infer means TS auto dhore nibe konta kon type (ex: string, boolean, number). Jodi auto infer korte na pare tahole amader manually bole dite hobe konta kon types.
+
+//  let myName:string;
+//  myName = 'sajid';
+
+// myName = 'sorker';
+
+// myName=34;
+// myName=false;
+
+// let age:number;
+// age = 34;
+// age = '45';
+
+
+// unionTypes means aker odhik type use kora jabe ak variable a. ar jonno age theke e bole dite hobe kon kon type rakha hobe.
+
+let age:string | number = 22; // akhane bole dewa hocche j string & number 2 tai rakha jabe
+age = '45'
+
+
+// const myName:'sajid' = 'sajid';
+let myName:'sajid' | 'sorker' = 'sajid';
+myName = "sorker"
