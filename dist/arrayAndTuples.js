822b0cea3feda7ba0a84dd9292a366fcd5c242cf
sajid365-sr <sajidsorker86@gmail.com>
2022-12-17 22:37:51 +0600

    array and tuples setup

---
+++ dist/arrayAndTuples.js
@@ -0,0 +1,27 @@
+"use strict";
+// Array and Tuples in typescript
+var players;
+players = ['Messi', 'Ronaldo'];
+players[0] = 'Neymar';
+// players = 34343;
+// players = false;
+players.push('Neymar');
+// players.push(2342)
+var newPlayers = players.map(function (player, i) { return i + 1 + ". " + player; });
+console.log(players, newPlayers);
+// Multiple types in an array
+var students;
+students = ['f', 's', 'z'];
+students = ['fdf', 343, 'dfdf'];
+students[0] = 4343;
+students.push(34);
+console.log(students);
+// any one types in an array
+var peoples;
+peoples = ['dfdf', 'dfsdfgd'];
+peoples = [true, false, true];
+// index wise types by "Tuples"
+var man;
+man = ['fdfd', 'dfdfd', 3553, false, 'gfdg'];
+man[0] = 'ldgjdsf';
+man[2] = 353;
---
+++ src/arrayAndTuples.ts
@@ -0,0 +1,43 @@
+
+// Array and Tuples in typescript
+
+let players: string[];
+
+players = [ 'Messi', 'Ronaldo'];
+
+players[0] = 'Neymar';
+
+// players = 34343;
+// players = false;
+
+players.push('Neymar');
+
+// players.push(2342)
+
+const newPlayers = players.map((player, i) => i + 1 + ". " + player);
+
+console.log(players, newPlayers);
+
+
+
+// Multiple types in an array
+let students:(string | number)[];
+students = ['f','s','z'];
+students = ['fdf', 343, 'dfdf'];
+students[0] = 4343;
+
+students.push(34);
+console.log(students);
+
+// any one types in an array
+let peoples:string[] | number[] | boolean[];
+peoples = ['dfdf', 'dfsdfgd'];
+peoples = [true, false, true];
+
+// index wise types by "Tuples"
+let man: [string, string, number, boolean, string ];
+man = ['fdfd', 'dfdfd', 3553, false, 'gfdg'];
+
+man[0] = 'ldgjdsf';
+man[2] = 353;
+
